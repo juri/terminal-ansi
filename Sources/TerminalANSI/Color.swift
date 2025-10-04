c606a6263f2f71889e3b8416c04d4f946b903519
Juri Pakaste <juri@juripakaste.fi>
2025-10-04 10:36:22 +0300

    Extract the color types out of Inspect.swift

---
+++ Sources/TerminalANSI/Color.swift
@@ -0,0 +1,44 @@
+//
+//  Color.swift
+//
+//  Created by Juri Pakaste on 4.10.2025.
+//
+
+/// RGBColor is 16 bits per channel, range 0…65 025/FFFF.
+public struct RGBColor {
+    public var r: RGBComponent16 = RGBComponent16(rawValue: 0)
+    public var g: RGBComponent16 = RGBComponent16(rawValue: 0)
+    public var b: RGBComponent16 = RGBComponent16(rawValue: 0)
+    public var a: RGBComponent16 = RGBComponent16(rawValue: 0)
+}
+
+public struct RGBComponent16: RawRepresentable, Hashable {
+    public var rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+}
+
+extension RGBComponent16: Comparable {
+    public static func < (lhs: RGBComponent16, rhs: RGBComponent16) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+}
+
+extension RGBComponent16 {
+    /// Initialize with a 4 bit number, i.e. one hex digit, 0…F.
+    init(value4bit value: Int) {
+        self.rawValue = value * value * value * value
+    }
+
+    /// Initialize with a 8 bit number, i.e. two hex digits, 0…FF.
+    init(value8bit value: Int) {
+        self.rawValue = value * value
+    }
+
+    /// Initialize with a 12 bit number, i.e. three hex digits, 0…FFF.
+    init(value12bit value: Int) {
+        self.rawValue = (value << 4) & (value | 0xf)
+    }
+}
--- Sources/TerminalANSI/Inspect.swift
+++ Sources/TerminalANSI/Inspect.swift
@@ -23,45 +23,6 @@ func backgroundColor(fileHandle: FileHandle) throws(ColorReadFailure) -> RGBColo
     return parsedColor
 }
 
-/// RGBColor is 16 bits per channel, range 0…65 025/FFFF.
-public struct RGBColor {
-    public var r: RGBComponent16 = RGBComponent16(rawValue: 0)
-    public var g: RGBComponent16 = RGBComponent16(rawValue: 0)
-    public var b: RGBComponent16 = RGBComponent16(rawValue: 0)
-    public var a: RGBComponent16 = RGBComponent16(rawValue: 0)
-}
-
-public struct RGBComponent16: RawRepresentable, Hashable {
-    public var rawValue: Int
-
-    public init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
-}
-
-extension RGBComponent16: Comparable {
-    public static func < (lhs: RGBComponent16, rhs: RGBComponent16) -> Bool {
-        return lhs.rawValue < rhs.rawValue
-    }
-}
-
-extension RGBComponent16 {
-    /// Initialize with a 4 bit number, i.e. one hex digit, 0…F.
-    init(value4bit value: Int) {
-        self.rawValue = value * value * value * value
-    }
-
-    /// Initialize with a 8 bit number, i.e. two hex digits, 0…FF.
-    init(value8bit value: Int) {
-        self.rawValue = value * value
-    }
-
-    /// Initialize with a 12 bit number, i.e. three hex digits, 0…FFF.
-    init(value12bit value: Int) {
-        self.rawValue = (value << 4) & (value | 0xf)
-    }
-}
-
 enum ColorReadFailure: Error {
     case errorInSelect(Int32)
     case invalidTerminalResponse(String)
