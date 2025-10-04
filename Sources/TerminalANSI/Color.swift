d5991d3383046a2366f7a8ea544a31d3bd575385
Juri Pakaste <juri@juripakaste.fi>
2025-10-04 11:16:10 +0300

    Add RGBAColor16 → RGBAColor8 scaling

--- Sources/TerminalANSI/Color.swift
+++ Sources/TerminalANSI/Color.swift
@@ -18,6 +18,22 @@ public struct RGBAColor16 {
     public var g: Component = Component(rawValue: 0)
     public var b: Component = Component(rawValue: 0)
     public var a: Component = Component(rawValue: 0)
+
+    public var scaledTo8: RGBAColor8 {
+        return RGBAColor8(
+            r: self.r.scaledTo8,
+            g: self.g.scaledTo8,
+            b: self.b.scaledTo8,
+            a: self.a.scaledTo8,
+        )
+    }
+}
+
+extension RGBAColor16.Component {
+    var scaledTo8: RGBAColor8.Component {
+        let scaledValue = UInt8(self.rawValue / 257)
+        return RGBAColor8.Component(rawValue: scaledValue)
+    }
 }
 
 extension RGBAColor16.Component: Comparable {
@@ -40,5 +56,26 @@ extension RGBAColor16.Component {
     /// Initialize with a 12 bit number, i.e. three hex digits, 0…FFF.
     init(value12bit value: Int) {
         self.rawValue = (value << 4) & (value | 0xf)
+    }
+}
+
+public struct RGBAColor8 {
+    public struct Component: RawRepresentable, Hashable {
+        public var rawValue: UInt8
+
+        public init(rawValue: UInt8) {
+            self.rawValue = rawValue
+        }
+    }
+
+    public var r: Component = Component(rawValue: 0)
+    public var g: Component = Component(rawValue: 0)
+    public var b: Component = Component(rawValue: 0)
+    public var a: Component = Component(rawValue: 0)
+}
+
+extension RGBAColor8.Component: Comparable {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.rawValue < rhs.rawValue
     }
 }
