8ba2f4951389bc86c53c6b28384c374c05c6a26a
Juri Pakaste <juri@juripakaste.fi>
2025-10-09 18:06:50 +0300

    Clean up Runner

--- Sources/RunTerminalANSI/Runner.swift
+++ Sources/RunTerminalANSI/Runner.swift
@@ -9,14 +9,24 @@ import TerminalANSI
 @main
 struct Runner {
     static func main() async throws {
-        guard let outTTY = Terminal() else {
+        guard let terminal = Terminal() else {
             return
         }
 
-        outTTY.writeCodes([
+        let isDark = try terminal.hasDarkBackground()
+
+        let size = try terminal.size()
+        terminal.writeCodes([
             .clearScreen,
-            .moveCursor(x: 20, y: 20),
-            .literal("hello world\n"),
+            .moveCursor(x: size.width / 2, y: size.height / 2 - 6),
+            .setGraphicsRendition([
+                .textRGB(isDark ? RGBColor8(intR: 0xD0, g: 0x80, b: 0xC0) : RGBColor8(intR: 0x80, g: 0x30, b: 0x60))
+            ]),
+            .literal("Hello world!\n"),
+            .moveCursor(x: 0, y: size.height),
+            .setGraphicsRendition([
+                .reset
+            ]),
         ])
     }
 }
